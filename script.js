e43b9691367564208b3d9bc77ae81c1291937c2e
atiks88 <141978852+atiks88@users.noreply.github.com>
2023-09-15 11:44:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-
+let complexArray = [
+  [
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    }
+  ]
+	[
+	{
+		true:true
+	},
+	{
+		false:false
+	}
+	]
+   [
+	   {
+		   undefined: undefined
+	   },
+	   {
+		   null:null
+	   }
+   ]
+];
+console.log(complexArray)
